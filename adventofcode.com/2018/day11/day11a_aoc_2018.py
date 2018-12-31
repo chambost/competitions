90ef6e81ed518b402140a1ce6540c6cd3d1c641e
Adam Chambost <chambost@users.noreply.github.com>
2018-12-31 17:24:08 +0800

    Original solution, AoC '18 11a
    Original solution at time of first correct submission (adventofcode.com 2018 day 11 Part 1)

---
+++ adventofcode.com/2018/day11/day11a_aoc_2018.py
@@ -0,0 +1,43 @@
+serial = 5177
+
+cells = [ [ 0 for i in range(300+1) ] for j in range(300+1) ]
+for y,row in enumerate(cells) :
+    if y == 0 :
+        continue
+    for x,cell in enumerate(row) :
+        if x == 0 : 
+            continue
+        rackid = x + 10
+        level = rackid * y
+        level += serial
+        level *= rackid
+        answer = (level // 100) % 10
+        answer -= 5
+        cells[y][x] = answer
+        
+def total(x,y) :
+    return cells[y][x]   + cells[y+1][x]   + cells[y+2][x] \
+         + cells[y][x+1] + cells[y+1][x+1] + cells[y+2][x+1] \
+         + cells[y][x+2] + cells[y+2][x+2] + cells[y+2][x+2]
+powers = [ (total(x,y),x,y) for x in range(1,300-1) for y in range(1,300-1) ]
+
+from collections import Counter
+c = Counter([p for p,_,_ in powers])
+maxp = max(c)
+print(max(c))
+
+print([(x,y) for p,x,y in powers if p == maxp])
+
+print(len(powers))
+print((300-2)*(300-2))
+
+print(max(powers))
+print(min(powers))
+print(c)
+
+coords = powers.index(max(powers))
+print(coords)
+print(coords % 300-2)
+print(coords // 300-2)
+print(total(236,22))
+
