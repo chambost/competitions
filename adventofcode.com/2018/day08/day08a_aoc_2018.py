6ae8cc8fa65a8aae4f83aec461f6d224565bb925
Adam Chambost <chambost@users.noreply.github.com>
2018-12-31 14:46:42 +0800

    Refactored to use comprehensions

--- adventofcode.com/2018/day08/day08a_aoc_2018.py
+++ adventofcode.com/2018/day08/day08a_aoc_2018.py
@@ -1,30 +1,19 @@
 input = ""
 
 class Node() :
-  def __init__(this, data) :
-    this.n_child = int(data.pop(0))
-    this.n_metadata = int(data.pop(0))
-    this.subnodes = []
-    for ii in range(this.n_child) :
-        this.subnodes.append( Node( data ) )
-    this.metadata = []
-    for jj in range(this.n_metadata) :
-        this.metadata.append( int(data.pop(0)) )
+  def __init__(self, data) :
+    n_child = int(data.pop(0))
+    n_metadata = int(data.pop(0))
+    self.children = [ Node(data) for _ in range(n_child) ]
+    self.metadata = [ int(data.pop(0)) for _ in range(n_metadata) ]
         
   def count(self) :
-    total = sum(self.metadata)
-    for child in self.subnodes :
-        total += child.count()
-    return total
+    return sum(self.metadata) + sum( [ child.count() for child in self.children ] )
+
+  def value(self) :            
+    return sum(self.metadata) if len(self.children) == 0 else self.childvalue()
 
   def childvalue(self) :
-    total = 0
-    for index in self.metadata :
-      if index != 0 and index <= self.n_child :
-        total += self.subnodes[index-1].value()
-    return total
-        
-  def value(self) :            
-    return sum(self.metadata) if self.n_child == 0 else self.childvalue()
+    return sum( self.children[index-1].value() if index != 0 and index <= len(self.children) else 0 for index in self.metadata )
     
 Node(input.split()).count()
