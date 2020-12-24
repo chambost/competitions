450d2907841c28fe1731441761b170535ba3e1bc
Adam Chambost <chambost@users.noreply.github.com>
2020-12-24 15:18:27 +0800

    Original solution, AoC 2020 Day 24, Part Two

---
+++ adventofcode.com/2020/day24/day24b_AoC_2020.py
@@ -0,0 +1,130 @@
+puzzleinput = r"""sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew"""
+
+from collections import deque
+from collections import Counter
+
+def parseline(line) :
+    d = deque(line)
+    while d :
+        first = d.popleft()
+        if first in {"s","n"} :
+            second = d.popleft()
+            yield "".join((first,second))
+        else :
+            yield first
+            
+def removeopposites(c) :
+    if c["w"] > c["e"] :
+        c["w"] -= c["e"]
+        c["e"] -= c["e"]
+    else :
+        c["e"] -= c["w"]
+        c["w"] -= c["w"]
+    if c["sw"] > c["ne"] :
+        c["sw"] -= c["ne"]
+        c["ne"] -= c["ne"]
+    else :
+        c["ne"] -= c["sw"]
+        c["sw"] -= c["sw"]
+    if c["nw"] > c["se"] :
+        c["nw"] -= c["se"]
+        c["se"] -= c["se"]
+    else :
+        c["se"] -= c["nw"]
+        c["nw"] -= c["nw"]
+        
+def removeloops(c) :
+    changed = True
+    while changed :
+        changed = False
+        if c["w"] > 0 and c["ne"] > 0 and c["se"] > 0 :
+            m = min(c["w"],c["ne"],c["se"])
+            c["w"] -= m
+            c["ne"] -= m
+            c["se"] -= m
+            changed = True
+        if c["e"] > 0 and c["nw"] > 0 and c["sw"] > 0 :
+            m = min(c["e"],c["nw"],c["sw"])
+            c["e"] -= m
+            c["nw"] -= m
+            c["sw"] -= m
+            changed = True
+            
+def shorten(c) :
+    if c["se"] > 0 and c["w"] > 0 :
+        m = min(c["se"],c["w"])
+        c["se"] -= m
+        c["w"] -= m
+        c["sw"] += m
+    if c["nw"] > 0 and c["sw"] > 0 :
+        m = min(c["nw"],c["sw"])
+        c["nw"] -= m
+        c["sw"] -= m
+        c["w"] += m
+    if c["ne"] > 0 and c["w"] > 0 :
+        m = min(c["ne"],c["w"])
+        c["ne"] -= m
+        c["w"] -= m
+        c["nw"] += m
+    if c["nw"] > 0 and c["e"] > 0 :
+        m = min(c["nw"],c["e"])
+        c["nw"] -= m
+        c["e"] -= m
+        c["ne"] += m
+    if c["ne"] > 0 and c["se"] > 0 :
+        m = min(c["ne"],c["se"])
+        c["ne"] -= m
+        c["se"] -= m
+        c["e"] += m
+    if c["e"] > 0 and c["sw"] > 0 :
+        m = min(c["e"],c["sw"])
+        c["e"] -= m
+        c["sw"] -= m
+        c["se"] += m
+
+bag = Counter()
+for line in puzzleinput.splitlines() :
+    c = Counter(parseline(line))
+    removeopposites(c)
+    removeloops(c)
+    shorten(c)
+    bag[tuple(sorted(c.elements()))] += 1
+for i in range(100) :
+    for item,count in bag.copy().items() :
+        if (count % 2) == 0 :
+            del bag[item]
+    counts = Counter()
+    for coord in bag.keys() :
+        for next in { "se","sw","w","nw","ne","e" } :
+            c = Counter(coord+(next,))
+            removeopposites(c)
+            removeloops(c)
+            shorten(c)
+            counts[tuple(sorted(c.elements()))] += 1
+    new_bag = Counter()
+    for item,count in counts.items() :
+        if item in bag and count in (1,2) :
+            new_bag[item] += 1
+        elif item not in bag and count == 2:
+            new_bag[item] += 1
+    bag = new_bag
+print(sum(bag.values()))
